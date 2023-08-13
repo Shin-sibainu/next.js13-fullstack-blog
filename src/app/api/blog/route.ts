6bd7a3ab2b5aae86db9f6c48cfa3da1d7e2f04ca
Shin-sibainu <shincode0712@gmail.com>
2023-08-13 18:38:06 +0900

    mod

---
+++ src/app/api/blog/route.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+// import prisma from "../../../../prisma";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  try {
+    await prisma.$connect();
+  } catch (err) {
+    return Error("DB接続に失敗しました");
+  }
+}
+
+export const GET = async (req: Request, res: NextResponse) => {
+  try {
+    await main();
+    const posts = await prisma.post.findMany();
+    return NextResponse.json({ message: "Success", posts }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+export const POST = async (req: Request, res: NextResponse) => {
+  console.log("POST");
+
+  try {
+    const { title, description } = await req.json();
+    await main();
+    const post = await prisma.post.create({ data: { title, description } });
+    return NextResponse.json({ message: "Success", post }, { status: 201 });
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
