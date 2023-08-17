2da872aeceb421dca444e1aca14fe4dc2e067deb
Shin-sibainu <shincode0712@gmail.com>
2023-08-17 14:29:01 +0900

    mod

--- copyLayout/Home.tsx
+++ copyLayout/Home.tsx
@@ -1,5 +1,5 @@
 <main className="w-full h-full">
-  <div className="md:w-2/4 sm:w-3/4 m-auto p-4 my-5 rounded-lg bg-slate-800 drop-shadow-xl">
+  <div className="md:w-2/4 sm:w-3/4 m-auto p-4 my-5 rounded-lg bg-blue-900 drop-shadow-xl">
     <h1 className="text-slate-200 text-center text-2xl font-extrabold">
       Full Stack Blog 📝
     </h1>
@@ -8,14 +8,14 @@
   <div className="flex my-5">
     <Link
       href={"/blog/add"}
-      className=" md:w-1/6 sm:w-2/4 text-center rounded-md p-2 m-auto bg-slate-200 font-semibold"
+      className=" md:w-1/6 sm:w-2/4 text-center rounded-md p-2 m-auto bg-slate-300 font-semibold"
     >
       ブログ新規作成
     </Link>
   </div>
 
   <div className="w-full flex flex-col justify-center items-center">
-    <div className="w-3/4 p-4 rounded-md mx-3 my-2 bg-slate-200 flex flex-col justify-center">
+    <div className="w-3/4 p-4 rounded-md mx-3 my-2 bg-slate-300 flex flex-col justify-center">
       <div className="flex items-center my-3">
         <div className="mr-auto">
           <h2 className="mr-auto font-semibold">First Post</h2>
